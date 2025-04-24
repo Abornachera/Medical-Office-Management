aad8cec38a4cbede74d176d9b123acd74fba11df
Anderson Bornachera <anderson@example.com>
2025-04-24 18:29:34 -0500

    Fix ApiError, estaba incompleto.

--- src/main/java/edu/unimagdalena/medicalofficemanagement/exception/ApiError.java
+++ src/main/java/edu/unimagdalena/medicalofficemanagement/exception/ApiError.java
@@ -1,21 +1,18 @@
 package edu.unimagdalena.medicalofficemanagement.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class ApiError {
-    private HttpStatus status;
-    private String message;
     private LocalDateTime timestamp;
-
-    public ApiError(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-        this.timestamp = LocalDateTime.now();
-    }
+    private String message;
+    private int status;
+    private Map<String, String> errors;
 }
