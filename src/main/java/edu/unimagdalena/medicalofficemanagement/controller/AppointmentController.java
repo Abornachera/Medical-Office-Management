3bd902f5233b2087a72f8a00eb922562f11468a4
Anderson Bornachera <anderson@example.com>
2025-04-24 18:35:01 -0500

    Removed unused import statement

--- src/main/java/edu/unimagdalena/medicalofficemanagement/controller/AppointmentController.java
+++ src/main/java/edu/unimagdalena/medicalofficemanagement/controller/AppointmentController.java
@@ -6,11 +6,9 @@ import edu.unimagdalena.medicalofficemanagement.model.Appointment;
 import edu.unimagdalena.medicalofficemanagement.service.AppointmentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
